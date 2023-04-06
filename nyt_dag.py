73ad57edd0301ffaaddd7144c932773b83eebb62
Felix Stiefel <fstiefel@indeed.com>
2023-04-06 13:56:51 +0100

    Split into multiple tasks

--- nyt_dag.py
+++ nyt_dag.py
@@ -4,7 +4,7 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime
-from nyt_etl import run_nyt_etl
+from nyt_etl import get_api_creds, read_nyt_api, transform_raw_nyt, write_data_to_s3
 
 
 
@@ -22,15 +22,38 @@ default_args = {
 dag = DAG(
     'nyt_dag',
     default_args=default_args,
-    description="NYT ETL process"
+    description="NYT ETL process",
+    schedule_interval='0 0 * * *'  # Runs every day at midnight
 )
 
 
-run_etl = PythonOperator(
-    task_id='complete_nyt_etl',
-    python_callable=run_nyt_etl,
-    dag=dag, 
+
+get_api_creds = PythonOperator(
+    task_id='get_api_creds',
+    python_callable=get_api_creds,
+    dag=dag,
 )
 
+read_nyt_api = PythonOperator(
+    task_id='read_nyt_api',
+    python_callable=read_nyt_api,
+    op_kwargs={'api_key': '{{ ti.xcom_pull(task_ids="get_api_creds") }}'},
+    dag=dag,
+)
 
-run_etl
+transform_raw_nyt = PythonOperator(
+    task_id='transform_raw_nyt',
+    python_callable=transform_raw_nyt,
+    op_kwargs={'data': '{{ ti.xcom_pull(task_ids="read_nyt_api") }}'},
+    dag=dag,
+)
+write_to_s3 = PythonOperator(
+    task_id='write_to_s3',
+    python_callable=write_data_to_s3,
+    op_kwargs={'df': '{{ ti.xcom_pull(task_ids="transform_raw_nyt") }}',
+               'start_date': '{{ ds }}'},
+    dag=dag,
+)
+
+get_api_creds >> read_nyt_api >> transform_raw_nyt >> write_to_s3
+
